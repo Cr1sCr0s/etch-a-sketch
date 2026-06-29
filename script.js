7e274241025e70a717a43014a890adc86083ea66
cris <crisrelayo17@gmail.com>
2026-06-29 14:29:05 +0800

    Add documentation

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const sizeBtn = document.querySelector('#grid-size');
 const container = document.querySelector('.container');
-let gridSize = 16;
-resizeGrid(gridSize, container);
+
+let gridSize = 16; // default size
+createGrid(gridSize, container); // initial grid
 
 sizeBtn.addEventListener('click', () => {
     gridSize = +(prompt('Enter grid size (min 1, max 100)', '16'));
+    // min 1, max 100
     if(gridSize > 100) gridSize = 100;
     if(gridSize < 1) gridSize = 1;
-    resizeGrid(gridSize, container);
+    createGrid(gridSize, container);
   }
 );
 
-function resizeGrid(gridSize, container){
-  while(container.firstChild){
+function createGrid(gridSize, container){
+  while(container.firstChild){ // empty grid
     container.removeChild(container.firstChild);
   }
   
+  // calculate min size of each box in percentage
   const minSize = ((1/gridSize) * 100) + '%';
 
   for(let i=0; i<(gridSize * gridSize); i++){
@@ -24,6 +27,7 @@ function resizeGrid(gridSize, container){
     box.style.minHeight = minSize;
     box.style.minWidth = minSize;
     box.addEventListener(
+      // random color on hover
       'mouseenter', () => box.style.backgroundColor = 'rgb(' 
       + getRandomInt(0, 255) + ',' 
       + getRandomInt(0, 255) + ',' 
@@ -33,6 +37,7 @@ function resizeGrid(gridSize, container){
   }
 }
 
+// function thanks to https://stackoverflow.com/a/1527820
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
