f2b579269a7e525542906c40b62f39768d545039
cris <crisrelayo17@gmail.com>
2026-06-29 11:51:50 +0800

    Add mouse hover effect for drawing in grid

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const container = document.querySelector('.container');
 for(let i=0; i<256; i++){
   const box = document.createElement('div');
   box.classList.toggle('box');
+  box.addEventListener('mouseenter', ()=>box.style.backgroundColor = 'black');
   container.appendChild(box);
 }
